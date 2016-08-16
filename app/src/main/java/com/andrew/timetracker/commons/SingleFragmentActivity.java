da3854ed351d7aa56a62cc6965936c4b3536c5af
diaus <diaus.gr@gmail.com>
2016-08-16 20:00:50 +0300

    Revert "testing author"
    This reverts commit 0bcaee25dbde54632a8104e9129a4849beac31df.

--- app/src/main/java/com/andrew/timetracker/commons/SingleFragmentActivity.java
+++ app/src/main/java/com/andrew/timetracker/commons/SingleFragmentActivity.java
@@ -20,6 +20,7 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView();
 
 		FragmentManager fm = getSupportFragmentManager();
