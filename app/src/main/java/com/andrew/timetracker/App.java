cd2954b6c4579a394202b685766c0c245bca2cde
diaus <diaus.gr@gmail.com>
2016-08-25 17:33:33 +0300

    Add localization for ukrainian language, fall-back ru to ukrainian

--- app/src/main/java/com/andrew/timetracker/App.java
+++ app/src/main/java/com/andrew/timetracker/App.java
@@ -1,12 +1,17 @@
 package com.andrew.timetracker;
 
 import android.app.Application;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
 
 import com.andrew.timetracker.database.DaoMaster;
 import com.andrew.timetracker.database.DaoSession;
 import com.andrew.timetracker.settings.Settings;
 
 import org.greenrobot.greendao.database.Database;
+
+import java.util.Locale;
 
 /**
  * Created by andrew on 15.08.2016.
@@ -21,12 +26,35 @@ public class App extends Application {
 	public void onCreate() {
 		super.onCreate();
 
+		setLocale();
+
 		DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, ENCRYPTED ? "timetracker-db-encrypted" : "timetracker-db");
 		Database db = ENCRYPTED ? helper.getEncryptedWritableDb("Ld3A3qNg2KJDrO6") : helper.getWritableDb();
 		daoSession = new DaoMaster(db).newSession();
 
 		// init singltons
 		Settings.init(this);
+	}
+
+	private void setLocale() {
+		Locale defaultLocale = Locale.getDefault();
+		String lang = defaultLocale.getLanguage();
+		if (!lang.equals("ru")) return;
+
+		Locale locale = new Locale("uk");
+		Locale.setDefault(locale);
+
+		Resources resources = getResources();
+
+		Configuration configuration = resources.getConfiguration();
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+			configuration.setLocale(locale);
+		} else {
+			configuration.locale = locale;
+		}
+
+		resources.updateConfiguration(configuration, resources.getDisplayMetrics());
 	}
 
 	public DaoSession getDaoSession() {
